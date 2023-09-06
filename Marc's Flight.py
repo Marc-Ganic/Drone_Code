5ccc7e7f9c954c544aee0f847cdec59f6246cc31
marc <marcganic@gmail.com>
2023-09-06 14:02:47 +0200

    Update to Marc's flight

--- Marc's Flight.py
+++ Marc's Flight.py
@@ -1,6 +1,11 @@
 from djitellopy import Tello
 import cv2
 
+width = 320
+height = 240
+startCounter = 0
+
+
 me = Tello()
 me.connect()
 me.for_back_velocity = 0
@@ -13,3 +18,21 @@ print(me.get_battery())
 
 me.streamoff()
 me.streamon()
+
+while True:
+
+    frame_read = me.get_frame_read()
+    myFrame = frame_read.frame
+    img = cv2.resize(myFrame, (width, height))
+
+    if startCounter == 0:
+        me.takeoff()
+        me.move_left(20)
+        me.rotate_clockwise(90)
+        startCounter = 1
+
+    cv2.imshow("MyResult", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        me.land()
+        break
