74f4cef3a413fcc007d54dba02622b18e77be3ae
KasperThorup25 <77360109+KasperThorup25@users.noreply.github.com>
2023-09-28 13:03:06 +0200

    it works!!

--- serial/read.py
+++ serial/read.py
@@ -3,6 +3,7 @@ import serial
 red_serial_port = "/dev/tty.usbserial-AH01H2QM"
 purple_serial_port = "/dev/tty.usbserial-A9014EXK"
 
+# declare object
 ser = serial.Serial(port=purple_serial_port, baudrate=115200)
 
 while True:
