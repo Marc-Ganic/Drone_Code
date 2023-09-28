056a3f1a0d9e1aa6fb2d85cd8acb89fabc6bd31f
KasperThorup25 <77360109+KasperThorup25@users.noreply.github.com>
2023-09-28 11:49:13 +0200

    new files

--- serial/read.py
+++ serial/read.py
@@ -1,8 +1,8 @@
-import read
+import serial
 
 ser = serial.Serial(port='/dev/tty.usbserial-AH01H2QM', baudrate=9600)
 
 while True:
-    value= serial.readline()
+    value= ser.readline()
     valueInstring=str(value, 'UTF8')
     print(valueInstring)
--- serial/write.py
+++ serial/write.py
@@ -1,7 +1,8 @@
 import serial
+import time
 
 ser = serial.Serial(port='/dev/tty.usbserial-AH01H2QM', baudrate=9600)
 
 while True:
-    ser.write("Hello world!")
-    sleep(1)
+    ser.write(1)
+    time.sleep(1)
